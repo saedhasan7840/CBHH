d3279042dc5c70d6275a391ef506401c9b0c8b5c
AK Saed <165506282+saedhasan7840@users.noreply.github.com>
2024-07-13 22:53:04 +0600

    Update Joker.py

--- Joker.py
+++ Joker.py
@@ -3,6 +3,7 @@ import os,sys,time,json,random,re,string,platform,base64,uuid
 os.system("git pull")
 from bs4 import BeautifulSoup as sop
 from bs4 import BeautifulSoup
+requests as ress
 from datetime import date
 from datetime import datetime
 from time import sleep
